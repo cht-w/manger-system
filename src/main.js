23d99bcb112d9f75bd684cd36bccc81406f18a2d
HaoTianChen <chthw513@163.com>
2022-03-08 00:25:47 +0800

    feat: 挂载storage

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import request from './utils/request'
+import storage from "./utils/storage";
 
 console.log('获取环境变量', import.meta.env)
 
@@ -12,4 +13,5 @@ const app = createApp(App);
 
 // 挂载全局的变量(和vue2稍有不同)
 app.config.globalProperties.$request = request;
+app.config.globalProperties.$storage = storage;
 app.use(router).use(ElementPlus).mount('#app')
