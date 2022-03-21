681e419c650c69f8e559e809699f330e21f82ac6
HaoTianChen <chthw513912@163.com>
2022-03-21 20:24:17 +0800

    欢迎页面的实现

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="welcome">
-    <h1>欢迎来到主界面</h1>
-    <router-link to="/login">去登陆</router-link>
+  <div class="welcome flex-center-all">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">通用后台管理系统</div>
+      <div class="desc text-left">
+        -Vue3 + koa2 + ElementPlus + MongoDB技术栈
+      </div>
+    </div>
+    <div class="img">
+      <img src="./../assets/images/welcome.png" alt="" />
+    </div>
   </div>
 </template>
 <script>
@@ -10,4 +18,34 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.welcome {
+  height: 100%;
+  .content {
+    position: relative;
+    bottom: 40px;
+    .sub-title {
+      font-size: 30px;
+      line-height: 42px;
+      color: #333;
+    }
+    .title {
+      font-size: 40px;
+      line-height: 62px;
+      color: #409eff;
+    }
+    .desc {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+  .img {
+    width: 371px;
+    height: 438px;
+    margin-left: 100px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
